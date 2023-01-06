f993394f978ed837e855ee34c2e398f809b681d9
Michael Whitney <mikew7224@gmail.com>
2023-01-05 19:52:04 -0600

    Add start of menu and system tray menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,10 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, SystemTray};
+use tauri::{ SystemTrayMenu, SystemTrayMenuItem};
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,8 +14,37 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let system_tray = create_system_tray_menu();
+    let menu = create_menu();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .system_tray(system_tray)
+        .menu(menu)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+    // let menu = construct_menu();
+    // tauri::Builder::default()
+    //     .menu(menu)    
+    //     .invoke_handler(tauri::generate_handler![greet])
+    //     .run(tauri::generate_context!())
+    //     .expect("error while running tauri application");
+}
+
+fn create_menu() -> Menu {
+    let quit = CustomMenuItem::new("quit".to_string(), "Exit");
+    let submenu = Submenu::new("File", Menu::new().add_item(quit));
+    let menu = Menu::new()
+        .add_submenu(submenu)
+        .add_item(CustomMenuItem::new("view", "View"));
+    return menu
+}
+
+fn create_system_tray_menu() -> SystemTray {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+      .add_item(quit)
+      .add_native_item(SystemTrayMenuItem::Separator)
+      .add_item(hide);
+    let tray = SystemTray::new().with_menu(tray_menu);
+    return tray;
 }
