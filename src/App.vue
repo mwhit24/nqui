a6556ef174ef1eebaeb6fab9ca708cf662a27a6f
mike-whitney <mikew7224@gmail.com>
2023-01-28 19:06:05 -0600

    conflict fixes

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,8 @@
 	<Layout/>
 </template>
 
-<script lang="ts" setup>
-import Layout from './components/ui/Layout.vue';</script>
-	<ButtonBar />
-    <div id="app-container" class="flex flex-row w-full justify-center px-4 py-2">
-	    <div id="left-drawer">
-		    <LeftDrawer />
-	    </div>
-        <div id="main-panel" class="w-full min-h-full">
-            <Main />
-        </div>
-        <div id="right-drawer">
-            <RightDrawer />
-        </div>
-    </div>
-</template>
-
 <script setup lang="ts">
-import Main from "./components/layout/Main.vue";
-import LeftDrawer from "./components/layout/LeftDrawer.vue";
-import RightDrawer from "./components/layout/RightDrawer.vue";
-import ButtonBar from "./components/layout/ButtonBar.vue";
+import Layout from './components/ui/Layout.vue';
 </script>
->>>>>>> 19078fe1b39ccd0e1b7eb15a686534ef625f8628
 
 
